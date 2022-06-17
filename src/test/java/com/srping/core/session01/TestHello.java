4de0c7b83854cc6f84c8d375b159f29e4c8984ee
student <student@BE-403->
2022-06-17 21:56:03 +0800

    Java Spring

---
+++ src/test/java/com/srping/core/session01/TestHello.java
@@ -0,0 +1,15 @@
+package com.srping.core.session01;
+
+import org.junit.Test;
+
+import com.srping.core.session01.beans.Hello;
+
+public class TestHello {
+
+	@Test
+	public void test() {
+		// 傳統上建立 Hello 物件
+		Hello h1 = new Hello();
+		System.out.println(h1);
+	}
+}
---
+++ src/test/java/com/srping/core/session01/TestHello2.java
@@ -0,0 +1,23 @@
+package com.srping.core.session01;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.srping.core.session01.beans.Hello;
+
+public class TestHello2 {
+	@Test
+	public void test() throws Exception {
+		// 利用 Spring 來取得所建立的 Hello 物件
+		// xml 配置 : conf/applicationContext.xml
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Hello hello = ctx.getBean(Hello.class);
+		System.out.println("hello: " + hello);
+
+		Thread.sleep(3000);
+
+		Hello hello2 = ctx.getBean("hello", Hello.class);
+		System.out.println("hello2: " + hello2);
+	}
+}
